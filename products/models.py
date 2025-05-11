97cfe00a6ba9105bbaa160f84755e996054f26b2
mildred-prog <mildylicious@gmail.com>
2025-05-11 23:21:45 +0100

    Customize admin product page

--- products/models.py
+++ products/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -10,6 +15,7 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
 
 
 class Product(models.Model):
