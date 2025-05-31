d0f03c4d96102083463149f10ecc9f3afbaaec57
Mildred <mildylicious@gmail.com>
2025-05-31 13:42:12 +0100

    Add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: boutique_ado.wsgi.application
--- products/models.py
+++ products/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
 
-    
     class Meta:
         verbose_name_plural = 'Categories'
         
@@ -15,7 +14,6 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
-
 
 
 class Product(models.Model):
