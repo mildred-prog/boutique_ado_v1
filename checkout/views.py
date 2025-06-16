84b6aa9f79c039762e3c7572726f8a6b5501892f
Mildred <mildylicious@gmail.com>
2025-06-16 16:35:55 +0100

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,9 @@
-
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -11,8 +13,18 @@ def checkout(request):
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+    
+    # Add debug logging
+    stripe_public_key = 'pk_test_51RadTXJy3jGoDeqvDwnOuf55mGRb0T1aBOOhTKlIJ3kdcBIj7PZM39QI0fVxEgGtqtyGalIASVKuqjyoyT3tWft400EOwcxfCw'
+    client_secret = 'test_client'
+    
+    logger.debug(f"Stripe public key: {stripe_public_key}")
+    logger.debug(f"Client secret: {client_secret}")
+    
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': client_secret,
     }
 
     return render(request, template, context)
